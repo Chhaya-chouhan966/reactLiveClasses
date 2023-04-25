eeaf6c714952ac68e28c282349932133f898e8d9
Chhaya-chouhan966 <107814362+Chhaya-chouhan966@users.noreply.github.com>
2023-04-26 00:52:58 +0530

    comp

--- start_react/src/Home6/Image/Footer/Footer1.jsx
+++ start_react/src/Home6/Image/Footer/Footer1.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer1 = () => {
   return (
     <div>
-        <h3>Footer</h3>
+        <h4>Footer</h4>
         <h5>this is my footer</h5>
         <p>Lorem ipsum dolor sit amet consectetur <br />
          adipisicing elit. At dolores beatae amet <br />
