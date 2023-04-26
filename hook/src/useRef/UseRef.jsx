e8766cfdc60f9214b2f1455f587a90d69bb49dee
Chhaya-chouhan966 <107814362+Chhaya-chouhan966@users.noreply.github.com>
2023-04-26 19:07:55 +0530

    useref

--- hook/src/useRef/UseRef.jsx
+++ hook/src/useRef/UseRef.jsx
@@ -1,8 +1,56 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 
 const UseRef = () => {
+
+  const [timer, setTimer] = useState(0)
+  let timerId = useRef() //timerId={current:undefined}
+  const href = useRef() //href={curret:undefiend} styling
+  const startBtn = useRef() //startBtn={curret:undefiend}
+  const stopBtn = useRef() //stopBtn={curret:undefiend}
+
+
+  useEffect(() => {
+    if (timer === 0) {
+      href.current.innerText = `Timer value is ${timer}`
+
+    } else {
+      href.current.innerText = `Timer started value is ${timer}`
+      href.current.style.color = "green"
+
+    }
+
+  })
+
+
+  const startTimer = () => {
+
+    startBtn.current.disabled = true
+    stopBtn.current.disabled = false
+
+
+    timerId.current = setInterval(() => {
+      setTimer(timer => timer + 1)
+    }, 1000)
+
+  }
+
+  const stopTimer = () => {
+   
+    href.current.innerText = `Timer stoped value is ${timer}`
+    href.current.style.color = "red"
+    startBtn.current.disabled = false
+    stopBtn.current.disabled = true
+
+    clearInterval(timerId.current)
+  }
+
   return (
-    <div>UseRef</div>
+    <div style={{ margin: "100px", textAlign: "center" }}>
+      <h2 ref={href}>{timer}</h2>
+      <br />
+      <button ref={startBtn} onClick={startTimer} style={{ padding: "0.5rem 2rem" }}>Start</button>&nbsp;&nbsp;&nbsp;
+      <button ref={stopBtn} onClick={stopTimer} style={{ padding: "0.5rem 2rem" }}>Stop</button>
+    </div>
   )
 }
 
